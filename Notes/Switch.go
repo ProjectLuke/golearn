28f0eb8fac2fc0fb5809349aebef59ac3343ce43
Luke Ugie <ugalinoluke@gmail.com>
2022-03-22 21:08:07 +0000

    Add notes about Switch

---
+++ Notes/Switch.go
@@ -0,0 +1,36 @@
+/************Switc*********/
+//*Switch is used to easily check multiple conditions
+//**Alternative to many if...else if blocks
+//*Switches execute from top-to-bottom
+//*Optionally have a 'default' action
+
+//*Having default allows something to be displayed
+//if none of the cases get checked
+
+/*Example*/
+
+package main
+import "fmt"
+
+x := 3
+switch x {
+case 1:
+	fmt.Println("1")
+case 2:
+	fmt.Println("2")
+case 3:
+	fmt.Println("3")
+default:
+	fmt.Println("other:", x)
+}
+
+
+url := "example.com"
+switch url {
+case "example.com":
+	fmt.Println("test")
+case "google.com"
+	fmt.Println("live")
+default:
+	fmt.Println("dev")
+}
