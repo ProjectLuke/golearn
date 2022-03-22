72655c2ab7c0b82fa583b5f18f5e44321850f610
Luke Ugie <ugalinoluke@gmail.com>
2022-03-22 21:07:51 +0000

    Add notes about Operators

---
+++ Notes/Operators.go
@@ -0,0 +1,42 @@
+//Arithmetic Operators
+/*
+   + Addition +=
+   - Subtraction -=
+   * Multiplication *=
+   / Division /=
+   % Remainder %=
+*/
+var total = 3 + 3
+
+//Arithmetic Assignment
+a := 1
+a += 3
+a = a + 3
+//Answer would obviously be 4
+//the a += 3 is equivalent to a = a + 3
+
+//Increment/Decrement
+i++
+i--
+
+//Arithmetic operators return a number
+
+//Relational Operators
+/*
+   < Less Than
+   <= Less Than or Equal To
+   > Greater Than
+   >= Greater Than or Equal To
+   == Equal To
+   != Not Equal To
+*/
+
+//Logic Operators
+/*
+   && And
+   || Or
+   ! Not
+*/
+
+//Nil
+//Nil means nothing
